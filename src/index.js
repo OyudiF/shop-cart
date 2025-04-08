f0d5a8942537f5e544e51fdd99568e1080a79718
Oyudi22 <ochiaifelipe@gmail.com>
2025-04-08 02:00:04 -0300

    final touches

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";   
 
-const cart = [];
+const myCart = [];
+const myWishList = [];
 
 console.log("Welcome to your shopping cart");
 
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
-console.log(item2.total()); 
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.remItem(myCart, item2);
+// await cartService.remItem(myCart, item2);
+
+await cartService.displayCart(myCart);
+
+
+await cartService.calTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,18 +1,56 @@
 // adicionar itens no carrinho
-async function addItem(userCart) {
-    
+async function addItem(userCart, item) {
+    userCart.push(item);    
 }
 
 // Deletar itens do carrinho
 async function delItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name);
 
+    if(index !== -1) {
+        userCart.splice(index, 1);
+    }
 }
 
 // remover itens do carrinho
-async function remItem(userCart, index) {
+async function remItem(userCart, item) {
+    const indexFound = userCart.findIndex((p) => p.name === item.name);
 
+    if(indexFound == -1) {
+        console.log("Item not found");
+        return;
+    }
+
+    if(userCart[indexFound].quantity > 1) {
+        userCart[indexFound].quantity -= 1;
+        return;
+    }
+
+    if(userCart[indexFound].quantity == 1) {
+        userCart.splice(indexFound, 1);
+        return;
+    }
 }
 
+// Calcula o total do carrinho
 async function calTotal(userCart) {
-    
+    const result = userCart.reduce((total, item)=> total + item.total(), 0);
+
+    console.log(`\nShopping cart total: R$${result}`);
+}
+
+async function displayCart(userCart) {
+    console.log("\nShopping cart list: ");
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | ${item.total()}`);
+    });
+}
+
+
+export {
+    addItem,
+    calTotal,
+    delItem,
+    remItem,
+    displayCart
 }
